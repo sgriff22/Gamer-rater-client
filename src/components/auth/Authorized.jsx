615209fcf857c273fc85613a465db33053f662c7
Stacy Griffy <sgriffy2@gmail.com>
2024-03-25 12:30:57 -0500

    Initial commit

---
+++ src/components/auth/Authorized.jsx
@@ -0,0 +1,16 @@
+import { Navigate, Outlet } from "react-router-dom";
+import { NavBar } from "../nav/Navbar.jsx";
+
+export const Authorized = () => {
+  if (localStorage.getItem("gamer_token")) {
+    return (
+      <>
+        <NavBar />
+        <main className="p-4">
+          <Outlet />
+        </main>
+      </>
+    );
+  }
+  return <Navigate to="/login" replace />;
+};
